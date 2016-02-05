4494a66831475bc129dc7e8d1609d09bb4c8f208
mikejcarn <mikejcarn@gmail.com>
2016-02-04 23:00:25 -0500

    apply texture to level; disable default gravity and enable gravity changing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
-
+	public float torque;
+	public bool isFlippable = false;
+	public float gravity = 9.81f;
+	private Vector3 upVector = new Vector3 (0, 1, 0); // The upwards vector, default is +y
 	private Rigidbody rb;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		rb.useGravity = false; // disable the default gravity
 	}
 
 	void FixedUpdate ()
 	{
+		/* manually apply gravity to the game object */
+		rb.velocity -= (upVector * gravity) * Time.deltaTime;
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
+		if (isFlippable) {
+			rb.AddTorque(-transform.up * torque * moveHorizontal, ForceMode.VelocityChange);
+			rb.AddTorque(transform.right * torque * moveVertical, ForceMode.VelocityChange);
+		}
 
+		rb.AddForce (movement * speed);
 	}
 
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag ("Platform"))
 		{
-			Application.LoadLevel(Application.loadedLevel);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+		if (other.gameObject.CompareTag ("Level1EndingTrigger"))
+		{
+			SceneManager.LoadScene(2);
+		}
+
+		if (other.gameObject.CompareTag ("Level2EndingTrigger"))
+		{
+			SceneManager.LoadScene(0);
 		}
 	}
 
